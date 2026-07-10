3dcb209d1dd46b51f1bdab4aadb68d9c8fe8a6a9
Abe Kornelis <abe@bixoft.nl>
2026-07-10 18:13:51 +0200

    Fix optionaltests not running when *All requested

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ bash/ivp
 rm -f ./z390test/build/z390test-output.txt
 # run the tests
 if [ "$(echo "$1" | tr '[:upper:]' '[:lower:]')" = "*all" ]; then
-  z390test/gradlew -p z390test cleanTest test optionalTest
+  z390test/gradlew -p z390test cleanTest test cleanOptionalTest optionalTest
 else
   z390test/gradlew -p z390test cleanTest test
 fi
